baf270e0291569e62618cef99ee6921f081a756f
four4444 <i@fourpro.ru>
2017-04-03 18:55:59 +0400

    online db

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
     host: "localhost:5000",
     dashboardEndpoint: "/dashboard",
-    dbURL: "mongodb://localhost/users"
+    dbURL: "mongodb://fourpro4444:Cthutq08121983@ds149700.mlab.com:49700/perman"
 }
